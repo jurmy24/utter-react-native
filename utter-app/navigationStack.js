f9a58c6e1a0b206af5f499a6472b94f38470ea6b
Hujon99 <joensson.hugo@gmail.com>
2023-12-05 16:43:13 +0100

    Misc: remove maincontainer from stack

--- utter-app/navigationStack.js
+++ utter-app/navigationStack.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingView from './Views/Intro/OnboardingView';
 import LoginView from './Views/Intro/LoginView';
+import MainContainer from './Views/Intro/MainContainerIntro';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,7 @@ function MyStack() {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Onboarding" component={OnboardingView} />
       <Stack.Screen name="Login" component={LoginView} />
-      {/* Add more screens here */}
+      {/* <Stack.Screen name="Intro" component={MainContainer}/> */}
     </Stack.Navigator>
   );
 }
