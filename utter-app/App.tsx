d4876c5d571c495b793b69c70b31c746d4eef326
Victor Oldensand <victormag24@gmail.com>
2023-12-06 15:46:48 +0100

    Build: create utterapp build with typescript and yarn

--- utter-app/App.tsx
+++ utter-app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Utter app</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
