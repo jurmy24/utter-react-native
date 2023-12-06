022fe7d48eb40c7d716ed167b1b2c6984b915c48
Hujon99 <joensson.hugo@gmail.com>
2023-12-06 09:51:25 +0100

    Feat: add home and chat view to navigation stack

--- utter-app/navigationStack.js
+++ utter-app/navigationStack.js
@@ -2,6 +2,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingView from './Views/Intro/OnboardingView';
 import LoginView from './Views/Intro/LoginView';
 import MainContainer from './Views/Intro/MainContainerIntro';
+import HomeView from './Views/Home/HomeView';
+import ChatView from './Views/Chat/ChatView';
+// import CallView from './Views/Call/CallView';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +13,9 @@ function MyStack() {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Onboarding" component={OnboardingView} />
       <Stack.Screen name="Login" component={LoginView} />
+      <Stack.Screen name="Home" component={HomeView} />
+      <Stack.Screen name="Chat" component={ChatView} />
+      {/* <Stack.Screen name="Call" component={CallView} /> */}
       {/* <Stack.Screen name="Intro" component={MainContainer}/> */}
     </Stack.Navigator>
   );
