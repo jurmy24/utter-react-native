cbd397e55a2bf77b79da189fede70d626a9d09e2
Victor Oldensand <victormag24@gmail.com>
2023-12-08 11:16:20 +0100

    Feat: build audio server controller, module, and service

--- server/src/audio/audio.module.ts
+++ server/src/audio/audio.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AudioController } from './audio.controller';
 import { AudioService } from './audio.service';
+import { AudioController } from './audio.controller';
 
 @Module({
-  controllers: [AudioController],
+  controllers: [AudioController], // Include this only if you have an AudioController
   providers: [AudioService],
+  exports: [AudioService] // Export AudioService if it will be used outside this module
 })
 export class AudioModule {}
+
