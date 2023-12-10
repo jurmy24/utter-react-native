a5adf444e68fd931569952501a2b8e7d8bda9df4
Victor Oldensand <victormag24@gmail.com>
2023-12-10 15:01:53 +0100

    Fix: correct audio playback for ios devices so that it's louder

--- utter-app/src/hooks/useSpeechSynth.js
+++ utter-app/src/hooks/useSpeechSynth.js
@@ -35,6 +35,18 @@ const useSpeechSynth = () => {
 
   const playAudio = async (filePath) => {
     try {
+      // Set the audio mode for playback
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+        playsInSilentModeIOS: true,
+        staysActiveInBackground: false,
+        shouldDuckAndroid: true,
+        playThroughEarpieceAndroid: false,
+        // interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        // interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      });
+
+      // Creating and playing the sound
       const { sound } = await Audio.Sound.createAsync({ uri: filePath });
       await sound.playAsync();
 
@@ -43,6 +55,17 @@ const useSpeechSynth = () => {
           // Audio playback finished
           await sound.unloadAsync();
           await FileSystem.deleteAsync(filePath);
+
+          // Reset to a default audio mode
+          await Audio.setAudioModeAsync({
+            allowsRecordingIOS: true, // or false, depending on your app's needs
+            playsInSilentModeIOS: true,
+            staysActiveInBackground: true, // or false, based on your preference
+            shouldDuckAndroid: true,
+            playThroughEarpieceAndroid: false,
+            // interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            // interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+          });
         }
       });
     } catch (error) {
