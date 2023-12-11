f5a109a7b94e9175f7208eb6c1c5ce1f06b75f54
Victor Oldensand <victormag24@gmail.com>
2023-12-11 11:46:46 +0100

    Fix: remove duplicate StyleSheet import in general styles

--- utter-app/src/assets/stylesheets/general_styles.js
+++ utter-app/src/assets/stylesheets/general_styles.js
@@ -1,5 +1,4 @@
 import { StyleSheet, Dimensions } from "react-native";
-import { StyleSheet, Dimensions } from "react-native";
 
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
