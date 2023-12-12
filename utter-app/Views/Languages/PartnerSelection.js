3b89e34e187dba275693f19992ee69fd1d83224b
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2023-12-12 10:44:40 +0100

    Feat: Add partners to language partner selection

--- utter-app/Views/Languages/PartnerSelection.js
+++ utter-app/Views/Languages/PartnerSelection.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import { View, SafeAreaView, ScrollView, Dimensions, TouchableOpacity, Text, ImageBackground} from 'react-native';
+import React, {useState} from 'react';
+import { 
+    View, 
+    SafeAreaView, 
+    ScrollView, 
+    TouchableOpacity, 
+    Text, 
+    ImageBackground, 
+    StyleSheet, 
+    Image
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import LanguagePartnerRow from '../Home/LanguagePartnerRow';
 import { generalStyles } from '../../assets/stylesheets/general_styles';
@@ -8,12 +17,162 @@ const assetsPath = "../../assets/";
 
 const PartnerSelection = () => {
 
+    const navigation = useNavigation();
+
+    // no partner is selected by default
+  const [selectedPartner, setSelectedPartner] = useState(null);
+
+  const handlePartnerRowPress = (partnerId) => {
+    // Handle the logic when a language row is pressed
+    setSelectedPartner(partnerId);
+  };
+
+  const saveSelectedLanguagePartner = (partnerId) => {
+    // Save to json
+  }
+
     return (
         <ImageBackground 
             source={require(assetsPath + 'images/slidingBackgroundWide.png')}
             style={generalStyles.background_style}
-        ></ImageBackground>
+        >
+
+            <SafeAreaView
+            style={{
+            ...generalStyles.safeArea,
+            paddingTop: Platform.OS === "android" ? 50 : 0,
+            flex: 0,
+            }}
+            >
+            </SafeAreaView>
+
+            <ScrollView style={styles.partnerListContainer}>
+                <View style={{ flex: 1 }}>
+                    {/* Exit Button */}
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Home')}
+                        style={styles.exitButton}
+                    >
+                        <Image source={require(assetsPath + "icons/X.png")} />
+                    </TouchableOpacity>
+
+                    {/* Go Back Button */}
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Languages')}
+                        style={styles.goBackButton}
+                    >
+                        <Image source={require(assetsPath + "icons/back-arrow.png")} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={[generalStyles.column, {alignItems: 'left'}]}>
+                    <Text style={[generalStyles.header, { marginTop: 100 }]}>
+                        I want to talk to
+                    </Text>
+                    <Text style={styles.mediumText}>Select language partner</Text>
+                </View>
+
+                {/* Language Partner Rows */}
+                <View style={styles.partnerList}>
+                    {/* This TouchableOpacity should update its state when pressed*/}
+                        <TouchableOpacity
+                            onPress={() => handlePartnerRowPress('english-chatbot')}
+                        >
+                            <LanguagePartnerRow/>
+                            
+                        </TouchableOpacity>
+                        <View style={styles.divider} />
+
+                    {/* This TouchableOpacity should update its state when pressed*/}
+                        <TouchableOpacity
+                            onPress={() => handlePartnerRowPress('french-chatbot')}
+                        >
+                            <LanguagePartnerRow/>
+                        </TouchableOpacity>
+
+                        <View style={styles.divider} />
+
+                        {/* Add more LanguagePartnerRows here as needed */}   
+                </View>
+                {/* Add Button at the bottom */}
+                <TouchableOpacity
+                    style={[
+                        styles.addButton,
+                        { backgroundColor: selectedPartner !== null ? '#536FFF' : '#CFD7FF' },
+                    ]}
+                    onPress={() => {/*Navigate to Home View and add language partner there*/}}
+                    disabled={selectedPartner === null} // Disable the button if language is not selected
+                >
+                    <Text style={styles.addButtonText}>Add</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        </ImageBackground>
     );
 };
+
+const styles = StyleSheet.create({
+    partnerListContainer: {
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        width: "100%",
+        paddingHorizontal: 0,
+        height: "100%",
+        // Add padding or margin as needed
+    },
+    partnerList: {
+        marginTop: 10, // Adjust as needed
+        backgroundColor: "#FFFFFF",
+        // Add shadow or other styles as needed
+    },
+    exitButton: {
+        position: "absolute",
+        top: 10,
+        right: 10,
+        width: 44,
+        height: 44,
+        borderRadius: 22,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      goBackButton: {
+        position: "absolute",
+        top: 10,
+        left: 10,
+        width: 44,
+        height: 44,
+        borderRadius: 22,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      mediumText: {
+        fontSize: 17,
+        marginBottom: 30,
+        marginTop: 10,
+      },
+      divider: {
+        height: 1,
+        backgroundColor: "lightgray",
+        marginHorizontal: 15,
+      },
+      addButton: {
+        borderRadius: 10,
+        backgroundColor: '#CFD7FF',
+        paddingVertical: 20,
+        marginLeft: 12,
+        marginRight: 12,
+        marginBottom: 12, // Adjust margin at the bottom
+        justifyContent: 'flex-end',
+      },
+    
+      addButtonText: {
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+});
 
 export default PartnerSelection;
